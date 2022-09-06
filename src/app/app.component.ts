d33cd6ec750c32867732d260e3a5f951c4e8278b
Luis99DavidFloresTorres <luisdavid99florestorres@gmail.com>
2022-09-06 11:36:58 -0400

    nuevos cambios1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,20 +9,17 @@ export class AppComponent {
   title = 'paginaSandrita';
   hrefvariable='https://api.whatsapp.com/send?phone=76122187&text=hola,%20¿qué%20tal?%20,cuánto%20cuesta%20';
   slides = [{'image': 'assets/pezCristiano.jpeg'}, {'image': 'assets/pezcristiano2.jpg'},{'image': 'assets/pezcristiano.jpg'}];
-  clase1:clases={titulo:"titulo1",imagen:'assets/pezCristiano.jpeg',descripcion:"descripcion1",precio:10.30};
-  clase2:clases={titulo:"titulo2",imagen:'assets/pezcristiano2.jpg',descripcion:"descripcion2",precio:12.30};
-  clase3:clases={titulo:"titulo1",imagen:'assets/pezCristiano.jpeg',descripcion:"descripcion1",precio:10.30};
-  clase4:clases={titulo:"titulo2",imagen:'assets/pezcristiano2.jpg',descripcion:"descripcion2",precio:12.30};
-  clase5:clases={titulo:"titulo1",imagen:'assets/pezCristiano.jpeg',descripcion:"descripcion1",precio:10.30};
-  clase6:clases={titulo:"titulo2",imagen:'assets/pezcristiano2.jpg',descripcion:"descripcion2",precio:12.30};
-  clase7:clases={titulo:"titulo2",imagen:'assets/pezcristiano2.jpg',descripcion:"descripcion2",precio:12.30};
-  clase8:clases={titulo:"titulo1",imagen:'assets/pezCristiano.jpeg',descripcion:"descripcion1",precio:10.30};
-  clase9:clases={titulo:"titulo2",imagen:'assets/pezcristiano2.jpg',descripcion:"descripcion2",precio:12.30};
-  clase10:clases={titulo:"titulo2",imagen:'assets/pezcristiano2.jpg',descripcion:"descripcion2",precio:12.30};
-  clase11:clases={titulo:"titulo1",imagen:'assets/pezCristiano.jpeg',descripcion:"descripcion1",precio:10.30};
-  clase12:clases={titulo:"titulo2",imagen:'assets/pezcristiano2.jpg',descripcion:"descripcion2",precio:12.30};
-  vector:clases[]=[this.clase1,this.clase2, this.clase3, this.clase4, this.clase5, this.clase6, this.clase7, this.clase8, this.clase8, this.clase9, this.clase10, this.clase11, this.clase12,
-  this.clase12,this.clase1,this.clase10];
+  clase1:clases={titulo:"titulo1",imagen:'assets/WhatsApp Image 2022-08-31 at 15.15.35.jpeg',descripcion:"descripcion1",precio:10.30};
+  clase2:clases={titulo:"titulo2",imagen:'assets/WhatsApp Image 2022-08-31 at 15.15.35 (1).jpeg',descripcion:"descripcion2",precio:12.30};
+  clase3:clases={titulo:"titulo1",imagen:'assets/WhatsApp Image 2022-08-31 at 15.15.36.jpeg',descripcion:"descripcion1",precio:10.30};
+  clase4:clases={titulo:"titulo2",imagen:'assets/WhatsApp Image 2022-08-31 at 15.15.36 (1).jpeg',descripcion:"descripcion2",precio:12.30};
+  clase5:clases={titulo:"titulo1",imagen:'assets/WhatsApp Image 2022-08-31 at 15.15.37.jpeg',descripcion:"descripcion1",precio:10.30};
+  clase6:clases={titulo:"titulo2",imagen:'assets/WhatsApp Image 2022-08-31 at 15.15.37 (1).jpeg',descripcion:"descripcion2",precio:12.30};
+  clase7:clases={titulo:"titulo2",imagen:'assets/WhatsApp Image 2022-08-31 at 15.15.38.jpeg',descripcion:"descripcion2",precio:12.30};
+  clase8:clases={titulo:"titulo1",imagen:'assets/WhatsApp Image 2022-08-31 at 15.15.38 (1).jpeg',descripcion:"descripcion1",precio:10.30};
+  clase9:clases={titulo:"titulo2",imagen:'assets/WhatsApp Image 2022-08-31 at 15.15.39.jpeg',descripcion:"descripcion2",precio:12.30};
+  vector:clases[]=[this.clase1,this.clase2, this.clase3, this.clase4, this.clase5, this.clase6, this.clase7, this.clase8, this.clase8, this.clase9,
+  this.clase1];
   mensajeClick(titulo:String){
     this.hrefvariable+=titulo+'?';
   }
